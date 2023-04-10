fd0df81f7681f14054ba66ee9758d596e724e0b9
we4p0n1zed <unw4p0n1zed@gmail.com>
2023-04-11 01:36:04 +0300

    update install function

--- Functions/Install-Office/Install-Office.ps1
+++ Functions/Install-Office/Install-Office.ps1
@@ -2,143 +2,145 @@
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mallockey/Install-Office365Suite/master/InstallOffice.psm1'))
 
-[CmdletBinding(DefaultParameterSetName = 'XMLFile')]
-param(
-  [Parameter(ParameterSetName = 'XMLFile')][String]$ConfigurationXMLFile,
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$AcceptEULA = 'TRUE',
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('SemiAnnualPreview', 'SemiAnnual', 'MonthlyEnterprise', 'CurrentPreview', 'Current')]$Channel = 'Current',
-  [Parameter(ParameterSetName = 'NoXML')][Switch]$DisplayInstall,
-  [Parameter(ParameterSetName = 'NoXML')][Switch]$IncludeProject,
-  [Parameter(ParameterSetName = 'NoXML')][Switch]$IncludeVisio,
-  [Parameter(ParameterSetName = 'NoXML')][Array]$LanguageIDs,
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('Groove', 'Outlook', 'OneNote', 'Access', 'OneDrive', 'Publisher', 'Word', 'Excel', 'PowerPoint', 'Teams', 'Lync')][Array]$ExcludeApps,
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('64', '32')]$OfficeArch = '64',
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('O365ProPlusRetail', 'O365BusinessRetail')]$OfficeEdition = 'O365ProPlusRetail',
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet(0, 1)]$SharedComputerLicensing = '0',
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$EnableUpdates = 'TRUE',
-  [Parameter(ParameterSetName = 'NoXML')][String]$SourcePath,
-  [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$PinItemsToTaskbar = 'TRUE',
-  [Parameter(ParameterSetName = 'NoXML')][Switch]$KeepMSI,
-  [String]$OfficeInstallDownloadPath = 'C:\Scripts\Office365Install',
-  [Switch]$CleanUpInstallFiles = $False
-)
+Function Install-Office365Suite {
+    [CmdletBinding(DefaultParameterSetName = 'XMLFile')]
+    param(
+        [Parameter(ParameterSetName = 'XMLFile')][String]$ConfigurationXMLFile,
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$AcceptEULA = 'TRUE',
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('SemiAnnualPreview', 'SemiAnnual', 'MonthlyEnterprise', 'CurrentPreview', 'Current')]$Channel = 'Current',
+        [Parameter(ParameterSetName = 'NoXML')][Switch]$DisplayInstall,
+        [Parameter(ParameterSetName = 'NoXML')][Switch]$IncludeProject,
+        [Parameter(ParameterSetName = 'NoXML')][Switch]$IncludeVisio,
+        [Parameter(ParameterSetName = 'NoXML')][Array]$LanguageIDs,
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('Groove', 'Outlook', 'OneNote', 'Access', 'OneDrive', 'Publisher', 'Word', 'Excel', 'PowerPoint', 'Teams', 'Lync')][Array]$ExcludeApps,
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('64', '32')]$OfficeArch = '64',
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('O365ProPlusRetail', 'O365BusinessRetail')]$OfficeEdition = 'O365ProPlusRetail',
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet(0, 1)]$SharedComputerLicensing = '0',
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$EnableUpdates = 'TRUE',
+        [Parameter(ParameterSetName = 'NoXML')][String]$SourcePath,
+        [Parameter(ParameterSetName = 'NoXML')][ValidateSet('TRUE', 'FALSE')]$PinItemsToTaskbar = 'TRUE',
+        [Parameter(ParameterSetName = 'NoXML')][Switch]$KeepMSI,
+        [String]$OfficeInstallDownloadPath = 'C:\Scripts\Office365Install',
+        [Switch]$CleanUpInstallFiles = $False
+    )
 
-$VerbosePreference = 'Continue'
-$ErrorActionPreference = 'Stop'
+    $VerbosePreference = 'Continue'
+    $ErrorActionPreference = 'Stop'
 
-$CurrentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (!($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
-  Write-Warning 'Script is not running as Administrator'
-  Write-Warning 'Please rerun this script as Administrator.'
-  exit
-}
+    $CurrentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if (!($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+        Write-Warning 'Script is not running as Administrator'
+        Write-Warning 'Please rerun this script as Administrator.'
+        exit
+    }
 
-if (-Not(Test-Path $OfficeInstallDownloadPath )) {
-  New-Item -Path $OfficeInstallDownloadPath -ItemType Directory | Out-Null
-}
+    if (-Not(Test-Path $OfficeInstallDownloadPath )) {
+        New-Item -Path $OfficeInstallDownloadPath -ItemType Directory | Out-Null
+    }
 
-if (!($ConfigurationXMLFile)) {
+    if (!($ConfigurationXMLFile)) {
 
-  if ($ExcludeApps) {
-    $OfficeXML = Get-XMLFile -AcceptEULA $AcceptEULA `
-      -Channel $Channel `
-      -DisplayInstall:$DisplayInstall `
-      -IncludeProject:$IncludeProject `
-      -IncludeVisio:$IncludeVisio `
-      -LanguageIDs $LanguageIDs `
-      -ExcludeApps $ExcludeApps `
-      -OfficeArch $OfficeArch `
-      -OfficeEdition $OfficeEdition `
-      -SharedComputerLicensing $SharedComputerLicensing `
-      -EnableUpdate $EnableUpdates `
-      -PinItemsToTaskBar $PinItemsToTaskbar `
-      -KeepMSI:$KeepMSI `
+        if ($ExcludeApps) {
+            $OfficeXML = Get-XMLFile -AcceptEULA $AcceptEULA `
+                -Channel $Channel `
+                -DisplayInstall:$DisplayInstall `
+                -IncludeProject:$IncludeProject `
+                -IncludeVisio:$IncludeVisio `
+                -LanguageIDs $LanguageIDs `
+                -ExcludeApps $ExcludeApps `
+                -OfficeArch $OfficeArch `
+                -OfficeEdition $OfficeEdition `
+                -SharedComputerLicensing $SharedComputerLicensing `
+                -EnableUpdate $EnableUpdates `
+                -PinItemsToTaskBar $PinItemsToTaskbar `
+                -KeepMSI:$KeepMSI `
   
-  }
-  else {
-    $OfficeXML = Get-XMLFile -AcceptEULA $AcceptEULA `
-      -Channel $Channel `
-      -DisplayInstall:$DisplayInstall `
-      -IncludeProject:$IncludeProject `
-      -IncludeVisio:$IncludeVisio `
-      -LanguageIDs $LanguageIDs `
-      -OfficeArch $OfficeArch `
-      -OfficeEdition $OfficeEdition `
-      -SharedComputerLicensing $SharedComputerLicensing `
-      -EnableUpdate $EnableUpdates `
-      -PinItemsToTaskBar $PinItemsToTaskbar `
-      -KeepMSI:$KeepMSI `
+        }
+        else {
+            $OfficeXML = Get-XMLFile -AcceptEULA $AcceptEULA `
+                -Channel $Channel `
+                -DisplayInstall:$DisplayInstall `
+                -IncludeProject:$IncludeProject `
+                -IncludeVisio:$IncludeVisio `
+                -LanguageIDs $LanguageIDs `
+                -OfficeArch $OfficeArch `
+                -OfficeEdition $OfficeEdition `
+                -SharedComputerLicensing $SharedComputerLicensing `
+                -EnableUpdate $EnableUpdates `
+                -PinItemsToTaskBar $PinItemsToTaskbar `
+                -KeepMSI:$KeepMSI `
   
-  }
+        }
 
-  $OfficeXML.Save("$OfficeInstallDownloadPath\OfficeInstall.xml")
+        $OfficeXML.Save("$OfficeInstallDownloadPath\OfficeInstall.xml")
 
-  $ConfigurationXMLFile = "$OfficeInstallDownloadPath\OfficeInstall.xml"
-}
-else {
+        $ConfigurationXMLFile = "$OfficeInstallDownloadPath\OfficeInstall.xml"
+    }
+    else {
 
-  if (!(Test-Path $ConfigurationXMLFile)) {
-    Write-Warning 'The configuration XML file is not a valid file'
-    Write-Warning 'Please check the path and try again'
-    exit
-  }
+        if (!(Test-Path $ConfigurationXMLFile)) {
+            Write-Warning 'The configuration XML file is not a valid file'
+            Write-Warning 'Please check the path and try again'
+            exit
+        }
 
-}
+    }
 
-$ODTInstallLink = Get-ODTURL
+    $ODTInstallLink = Get-ODTURL
 
-#Download the Office Deployment Tool
-Write-Verbose 'Downloading the Office Deployment Tool...'
-try {
-  Invoke-WebRequest -Uri $ODTInstallLink -OutFile "$OfficeInstallDownloadPath\ODTSetup.exe"
-}
-catch {
-  Write-Warning 'There was an error downloading the Office Deployment Tool.'
-  Write-Warning 'Please verify the below link is valid:'
-  Write-Warning $ODTInstallLink
-  exit
-}
+    #Download the Office Deployment Tool
+    Write-Verbose 'Downloading the Office Deployment Tool...'
+    try {
+        Invoke-WebRequest -Uri $ODTInstallLink -OutFile "$OfficeInstallDownloadPath\ODTSetup.exe"
+    }
+    catch {
+        Write-Warning 'There was an error downloading the Office Deployment Tool.'
+        Write-Warning 'Please verify the below link is valid:'
+        Write-Warning $ODTInstallLink
+        exit
+    }
 
-#Run the Office Deployment Tool setup
-try {
-  Write-Verbose 'Running the Office Deployment Tool...'
-  Start-Process "$OfficeInstallDownloadPath\ODTSetup.exe" -ArgumentList "/quiet /extract:$OfficeInstallDownloadPath" -Wait
-}
-catch {
-  Write-Warning 'Error running the Office Deployment Tool. The error is below:'
-  Write-Warning $_
-}
+    #Run the Office Deployment Tool setup
+    try {
+        Write-Verbose 'Running the Office Deployment Tool...'
+        Start-Process "$OfficeInstallDownloadPath\ODTSetup.exe" -ArgumentList "/quiet /extract:$OfficeInstallDownloadPath" -Wait
+    }
+    catch {
+        Write-Warning 'Error running the Office Deployment Tool. The error is below:'
+        Write-Warning $_
+    }
 
-#Run the O365 install
-try {
-  Write-Verbose 'Downloading and installing Microsoft 365'
-  $Silent = Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfigurationXMLFile" -Wait -PassThru
-}
-catch {
-  Write-Warning 'Error running the Office install. The error is below:'
-  Write-Warning $_
-}
+    #Run the O365 install
+    try {
+        Write-Verbose 'Downloading and installing Microsoft 365'
+        $Silent = Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfigurationXMLFile" -Wait -PassThru
+    }
+    catch {
+        Write-Warning 'Error running the Office install. The error is below:'
+        Write-Warning $_
+    }
 
-#Check if Office 365 suite was installed correctly.
-$RegLocations = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-  'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-)
+    #Check if Office 365 suite was installed correctly.
+    $RegLocations = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+        'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    )
 
-$OfficeInstalled = $False
-foreach ($Key in (Get-ChildItem $RegLocations) ) {
-  if ($Key.GetValue('DisplayName') -like '*Microsoft 365*') {
-    $OfficeVersionInstalled = $Key.GetValue('DisplayName')
-    $OfficeInstalled = $True
-  }
-}
+    $OfficeInstalled = $False
+    foreach ($Key in (Get-ChildItem $RegLocations) ) {
+        if ($Key.GetValue('DisplayName') -like '*Microsoft 365*') {
+            $OfficeVersionInstalled = $Key.GetValue('DisplayName')
+            $OfficeInstalled = $True
+        }
+    }
 
-if ($OfficeInstalled) {
-  Write-Verbose "$($OfficeVersionInstalled) installed successfully!"
-}
-else {
-  Write-Warning 'Microsoft 365 was not detected after the install ran'
-}
+    if ($OfficeInstalled) {
+        Write-Verbose "$($OfficeVersionInstalled) installed successfully!"
+    }
+    else {
+        Write-Warning 'Microsoft 365 was not detected after the install ran'
+    }
 
-if ($CleanUpInstallFiles) {
-  Remove-Item -Path $OfficeInstallDownloadPath -Force -Recurse
+    if ($CleanUpInstallFiles) {
+        Remove-Item -Path $OfficeInstallDownloadPath -Force -Recurse
+    }
 }
